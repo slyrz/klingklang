7cbb5129c1e539b0f67812473e385f64b460d4db
slyrz <noreply@localhost>
2013-12-11 16:25:40 +0100

    making way for OpenBSD support

--- src/list.c
+++ src/list.c
@@ -1,19 +1,37 @@
 #include <klingklang/list.h>
 #include <klingklang/util.h>
 
+#ifdef HAVE_QSORT_R
 static int _kk_list_compare (const void *a, const void *b, void *arg);
+#else
+static int _kk_list_compare (const void *a, const void *b);
+#endif
+
 static int _kk_list_enlarge (kk_list_t *list);
 
-/**
- * kk_list_t stores void*pointers. Calling qsort,
- * we receive pointers to those pointers in this function. Function parameters
- * are declared void*to not break the qsort compare function prototype. 
- * So we cast them to void**, dereference them and cast them to
- * actual void*pointers and call the user-defined compare function.
- */
+#ifndef HAVE_QSORT_R
+#  include <pthread.h>
+#endif
+
+#ifndef HAVE_QSORT_R
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static void *arg = NULL;
+#endif
+
 static int
+#ifdef HAVE_QSORT_R
 _kk_list_compare (const void *a, const void *b, void *arg)
+#else
+_kk_list_compare (const void *a, const void *b)
+#endif
 {
+  /**
+   * kk_list_t stores void* pointers. Calling qsort,
+   * we receive pointers to those pointers in this function. Function parameters
+   * are declared void*to not break the qsort compare function prototype. 
+   * So we cast them to void**, dereference them and cast them to
+   * actual void*pointers and call the user-defined compare function.
+   */
   return ((kk_list_cmp_f) arg) (*((const void *const *) a), *((const void *const *) b));
 }
 
@@ -92,7 +110,18 @@ kk_list_append (kk_list_t *list, void *item)
 int
 kk_list_sort (kk_list_t *list, kk_list_cmp_f cmp)
 {
-  if (list->len)
-    qsort_r (list->items, list->len, sizeof (void *), _kk_list_compare, cmp);
+  if (list->len <= 1)
+    return 0;
+
+#ifdef HAVE_QSORT_R
+  qsort_r (list->items, list->len, sizeof (void *), _kk_list_compare, cmp);
+#else
+  pthread_mutex_lock (&mutex);
+  arg = cmp;
+  qsort (list->items, list->len, sizeof (void *), _kk_list_compare);
+  arg = NULL;
+  pthread_mutex_unlock (&mutex);
+#endif
+
   return 0;
 }
